db0b58551d2cbc29bc15b6061889f58f92464a9a
Maximilian Hammes <maximview@gmail.com>
2024-07-11 23:07:04 +0200

    add active icon class and background

--- js/index.js
+++ js/index.js
@@ -6,3 +6,28 @@ document.querySelectorAll(".button").forEach((button) => {
     button.textContent = hide ? "Hide answer" : "Show answer";
   });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const icons = document.querySelectorAll(".navigation-bar .icon");
+  const removeAllActive = () => {
+    icons.forEach((i) => i.classList.remove("active"));
+  };
+
+  icons.forEach((icon) => {
+    icon.addEventListener("click", (event) => {
+      event.preventDefault();
+
+      removeAllActive();
+
+      icon.classList.add("active");
+    });
+  });
+
+  const currentPage = window.location.pathname;
+  icons.forEach((icon) => {
+    // Vergleichen des href jedes Icons mit dem aktuellen Seitenpfad
+    if (icon.parentElement.pathname === currentPage) {
+      icon.classList.add("active");
+    }
+  });
+});
